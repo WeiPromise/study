518615818269e861f33f4107f6d67671cd683b8d
lei wei <16itdream@gmail.com>
2020-08-12 17:33:29 +0800

    first commit

---
+++ day01_注释/demo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3.5
+# encoding: utf-8
+# Created by leiwei on 2020/8/12 17:08
+
+# 单行注释
+
+# print("hello python")
+
+#多行注释
+
+'''
+print("hello python")
+print("hello python")
+'''
+
+
+
+
+
